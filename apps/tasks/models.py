e839f12d1a6f5eeed81449c5ac5418eede161437
artio <artiom.carter.03@mail.ru>
2022-08-03 17:17:35 +0300

    Refactor imports

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,10 +1,10 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-from datetime import datetime
 
 from config import settings
 
