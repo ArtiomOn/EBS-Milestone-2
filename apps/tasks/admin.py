a06b58f466a2b4584b1f4fd7f9a690071b48c9cc
artio <artiom.carter.03@mail.ru>
2022-08-03 15:51:06 +0300

    Add action send_email to users

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.core.mail import send_mail
 
 from apps.tasks.models import (
     Task,
     Comment,
     TimeLog,
 )
+from config import settings
+
+
+@admin.action(description='Send email to user')
+def send_user_email(model_admin, request, queryset):
+    queryset = queryset.select_related('assigned_to').values_list('assigned_to__email', flat=True)
+    send_mail(
+        message='test message from django admin',
+        subject='test subject from django admin',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=list(queryset),
+        fail_silently=False
+    )
 
 
 @admin.register(Task)
 class TaskAdmin(ModelAdmin):
     list_display = ('id', 'title', 'status', 'assigned_to')
+    actions = [send_user_email]
+
+    def save_related(self, request, form, formsets, change):
+        task_id = form.instance.id
+        queryset = super(TaskAdmin, self).get_queryset(request).filter(pk=task_id)
+        user_email = queryset.select_related('assigned_to').values_list('assigned_to__email', flat=True)
+        change_data = form.changed_data
+        status = queryset.values_list('status', flat=True).filter().first()
+        if change_data == ['status'] and status is False:
+            send_mail(
+                message=f'Admin changed you task status to Undone!',
+                subject=f'You have one undone Task. Id:{task_id}',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=list(user_email),
+                fail_silently=False
+            )
+        return super(TaskAdmin, self).save_related(request, form, formsets, change)
 
 
 @admin.register(Comment)
