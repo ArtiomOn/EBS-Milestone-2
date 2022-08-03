7c6944ffaec15209849724b02170bffefe3f0af4
artio <artiom.carter.03@mail.ru>
2022-08-03 15:51:52 +0300

    Rename email message and subject

--- apps/tasks/tasks.py
+++ apps/tasks/tasks.py
@@ -5,6 +5,10 @@ from django.core.mail import send_mail
 
 from config import settings
 
+__all__ = [
+    'send_email'
+]
+
 
 @shared_task
 def send_email():
@@ -12,8 +16,8 @@ def send_email():
     user_email = set(Task.objects.select_related(
         'assigned_to').filter(status=False).values_list('assigned_to__email', flat=True))
     send_mail(
-        message='test Celery',
-        subject='test Celery',
+        message='Your task is not completed',
+        subject='Your task in not completed',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=list(user_email),
         fail_silently=False
