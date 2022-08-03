0d9c9a1450a37fc1a0803c3a4a5f9303f74eac69
artio <artiom.carter.03@mail.ru>
2022-08-03 17:16:41 +0300

    Add save model for TaskAdmin, CommentAdmin, TimeLogAdmin

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -27,29 +27,36 @@ class TaskAdmin(ModelAdmin):
     list_display = ('id', 'title', 'status', 'assigned_to')
     actions = [send_user_email]
 
-    def save_related(self, request, form, formsets, change):
-        task_id = form.instance.id
-        queryset = super(TaskAdmin, self).get_queryset(request).filter(pk=task_id)
-        change_data = form.changed_data
-        status = form.instance.status
-        if 'status' in change_data and status is False:
-            user_email = queryset.select_related('assigned_to').values_list('assigned_to__email', flat=True)
+    def save_model(self, request, obj, form, change):
+        update_fields = []
+        for key, value in form.cleaned_data.items():
+            if value != form.initial[key]:
+                update_fields.append(key)
 
-            send_mail(
-                message=f'Admin changed you task status to Undone!',
-                subject=f'You have one undone Task. Id:{task_id}',
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=list(user_email),
-                fail_silently=False
-            )
-        return super(TaskAdmin, self).save_related(request, form, formsets, change)
+        obj.save(update_fields=update_fields)
 
 
 @admin.register(Comment)
 class CommentAdmin(ModelAdmin):
     list_display = ('id', 'task_id', 'assigned_to')
 
+    def save_model(self, request, obj, form, change):
+        update_fields = []
+        for key, value in form.cleaned_data.items():
+            if value != form.initial[key]:
+                update_fields.append(key)
+
+        obj.save(update_fields=update_fields)
+
 
 @admin.register(TimeLog)
 class TimeLogAdmin(ModelAdmin):
     list_display = ('id', 'task', 'user', 'started_at', 'duration')
+
+    def save_model(self, request, obj, form, change):
+        update_fields = []
+        for key, value in form.cleaned_data.items():
+            if value != form.initial[key]:
+                update_fields.append(key)
+
+        obj.save(update_fields=update_fields)
