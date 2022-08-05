e321ae6c03aa504be32f22cebf1c398e3a0dc0ae
artio <artiom.carter.03@mail.ru>
2022-08-05 12:47:01 +0300

    Add FileViewSet to url

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path, include
-from rest_framework_nested.routers import NestedSimpleRouter, DefaultRouter
+
+from rest_framework_nested.routers import (
+    NestedSimpleRouter,
+    DefaultRouter
+)
 
 from apps.tasks.views import (
     TaskViewSet,
     TaskCommentViewSet,
     TaskTimeLogViewSet,
-    TimeLogViewSet
+    TimeLogViewSet,
+    FileViewSet,
 )
 
 
 base_router = DefaultRouter()
 base_router.register(r'tasks', TaskViewSet, basename='tasks')
 base_router.register(r'timelogs', TimeLogViewSet, basename='timelogs')
+base_router.register(r'files', FileViewSet, basename='files')
 
 nested_router = NestedSimpleRouter(base_router, r'tasks', lookup='task')
 nested_router.register(r'comments', TaskCommentViewSet, basename='comments')
