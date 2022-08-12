6007380d5fd8329d46991ee5cf06ae65f7e58a1d
artio <artiom.carter.03@mail.ru>
2022-08-12 11:10:32 +0300

    Add Project model

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -9,20 +9,59 @@ from django.dispatch import receiver
 from config import settings
 from config.settings import AUTH_USER_MODEL as User
 
-
 __all__ = [
     'Task',
     'Comment',
     'TimeLog',
-    'Attachment'
+    'Attachment',
+    'Project'
 ]
+
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=150,
+        unique=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    logo = models.ManyToManyField(
+        'Attachment',
+        blank=True
+    )
+    description = models.TextField(
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
     title = models.CharField(
         max_length=50,
     )
-    description = models.TextField()
+    description = models.TextField(),
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        null=True
+    )
     status = models.BooleanField(
         default=False,
         verbose_name='Completed'
