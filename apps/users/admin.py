52bd98ad7b48e4f1385ac750bab8af5d00b22d2e
artio <artiom.carter.03@mail.ru>
2022-08-12 16:16:03 +0300

    Add save_model to CustomUserAdmin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.db.models import QuerySet
 
 from apps.users.models import CustomUser
 
@@ -7,3 +8,15 @@ from apps.users.models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff')
+
+    def save_model(self, request, obj, form, change):
+        ignored_keys = []
+        update_fields = []
+        if bool(form.initial):
+            for key, value in form.cleaned_data.items():
+                if isinstance(value, QuerySet):
+                    ignored_keys.append(key)
+                if value != form.initial[key]:
+                    update_fields.append(key)
+            return obj.save(update_fields=set(update_fields) - set(ignored_keys))
+        return super(CustomUserAdmin, self).save_model(request, obj, form, change)
