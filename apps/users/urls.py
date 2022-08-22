6c8e04d7356978c57ebb27794c59a50671c4dcb7
artio <artiom.carter.03@mail.ru>
2022-08-22 17:00:44 +0300

    Add TokenRefreshView

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
+    TokenObtainPairView,
 )
 from rest_framework_nested.routers import DefaultRouter
 
@@ -19,13 +19,13 @@ urlpatterns = base_router.urls
 
 urlpatterns += [
     path(
-        'token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
         'token/refresh/',
         TokenRefreshView.as_view(),
         name='token_refresh'
     ),
+    path(
+        'token/access/',
+        TokenObtainPairView.as_view(),
+        name='token_access'
+    )
 ]
