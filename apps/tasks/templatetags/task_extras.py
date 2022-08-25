9e9e2f5717d5b63faa9bd18b5ad1249109aa5508
artio <artiom.carter.03@mail.ru>
2022-08-25 09:32:54 +0300

    Update

--- apps/tasks/templatetags/task_extras.py
+++ apps/tasks/templatetags/task_extras.py
@@ -2,10 +2,5 @@ from django.template.loader_tags import register
 
 
 @register.filter
-def in_task(comment, task):
-    return comment.filter(task=task)
-
-
-@register.filter
-def in_timelog(timelog, task):
-    return timelog.filter(task=task)
+def in_task(obj, task):
+    return obj.filter(task=task)
