5947860a5134659019060d4e843c246db0db279e
artio <artiom.carter.03@mail.ru>
2022-08-29 15:41:27 +0300

    Add admin_honeypot

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
     'minio_storage',
+    'admin_honeypot',
 
     'apps.common',
     'apps.tasks',
--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('auth/', admin.site.urls),
     path('', include('apps.tasks.urls')),
     path('users/', include("apps.users.urls")),
     path('accounts/', include('allauth.urls')),
@@ -28,5 +29,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
